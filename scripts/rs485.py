017ffd4f346971c7d2703f3f19064ac11c490562
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2018-10-13 13:14:22 +0530

    esp32 starter

--- scripts/rs485.py
+++ scripts/rs485.py
@@ -1,6 +1,9 @@
 import serial
 
-s = serial.Serial('/dev/cu.wchusbserial1420')
+COM_PORT = '/dev/cu.wchusbserial1420'
+
+
+s = serial.Serial(COM_PORT)
 while True:
     s.write("welcome".encode())
     res = s.readline()
